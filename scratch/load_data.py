21ef48b0daade5cfa88ff17c377353307a341d62
Anirudh Swaminathan <aniswami97@gmail.com>
2021-02-06 15:30:30 -0800

    Pick up nationalities
    Need to draw pie chart for the same

--- scratch/load_data.py
+++ scratch/load_data.py
@@ -9,6 +9,16 @@ def main():
     # dat = np.load("../../../datasets/fifa19/data.csv", allow_pickle=True)
     dat = pd.read_csv("../../../datasets/fifa19/data.csv")
     print(dat.shape)
+    print(type(dat))
+    headers = dat.head()
+    print(type(headers), headers.shape)
+    col_names = list(dat.columns.values)
+    print(len(col_names))
+    print(col_names[:5], col_names[-5:])
+    nations = dat['Nationality']
+    print(nations.shape)
+    nations.apply(pd.value_counts).plot.pie(subplots=True)
+    plt.show()
 
 if __name__ == '__main__':
     main()
